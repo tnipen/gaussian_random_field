6dbad82cdffddc3f898ccf2c6f559e0bee2a4e46
Tjøtta Erik <erik.tjotta@statkraft.com>
2019-12-19 17:18:20 +0100

    Adapting to Python 3, adding 'keep_ec_values' option and correcting number of target hours to T2=(T-1)*n

--- gaussian_random_field.py
+++ gaussian_random_field.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib.pylab as mpl
+# import matplotlib.pylab as mpl
+import matplotlib.pyplot as plt
 import scipy.interpolate
 import sys
 
@@ -17,9 +18,10 @@ import sys
     Returns:
         array (np.array): 3D array (X, Y, Time)
 """
-def downscale_field(array, n, rx, rt, a):
+def downscale_field(array, n, rx, rt, a, keep_ec_values=False):
     X, Y, T = array.shape
-    T2 = T * n
+    # T2 = T * n
+    T2 = (T - 1) * n
 
     # Linearly interpolate the forecast to new time dimension
     func = scipy.interpolate.interp1d(np.linspace(0, 1, T), array, axis=2)
@@ -29,10 +31,14 @@ def downscale_field(array, n, rx, rt, a):
 
     """ Adjust the field by adding the noise. This is done in log windspeed space.
     """
-    new_array = np.exp(np.log(ret) + noise * a)
-    # new_array = noise
+    if keep_ec_values:
+        time_dependent_weights = np.sin(np.arange(0,n)/n * np.pi)
+    else:
+        time_dependent_weights = np.ones(n)
+    time_dependent_std = a * np.tile(time_dependent_weights, (X, Y, T2//n))
+    new_array = np.exp(np.log(ret) + np.multiply(noise, time_dependent_std))
 
-    return new_array
+    return new_array, ret
 
 
 """ Creates a gaussian random field with given spatial/temporal characteristics and a stdev of 1
@@ -76,32 +82,41 @@ def Pk(kx, ky, kt, rx, rt):
 
 """ Not sure what this does ... """
 def fftIndgen(n):
-    a = range(0, n/2+1)
-    b = range(1, n/2)
+    # a = range(0, n/2+1)
+    # b = range(1, n/2)
+    # b.reverse()
+    # b = [-i for i in b]
+    a = list(range(0, n//2+1))
+    b = list(range(1, n//2))
     b.reverse()
     b = [-i for i in b]
     return a + b
 
 
-np.random.seed(1000)
+np.random.seed(1001)
 
 # Create a EC wind forecast with 5 timesteps of 6 hours
-raw_forecast = 3 * np.ones([100,100,5])
+raw_forecast = 3 * np.ones([100, 100, 5])
 
 # Downscale to 1 hour resolution
-x = downscale_field(raw_forecast, n=6, rx=4, rt=2, a=0.1)
+x, raw_intp = downscale_field(raw_forecast, n=6, rx=4, rt=0.5, a=0.1, keep_ec_values=False)
 
 T = x.shape[2]
 for i in range(T):
-    mpl.subplot(int(np.sqrt(T)), T / int(np.sqrt(T))+1,i+1)
-    mpl.pcolormesh(x[:, :, i], vmin=np.min(x[:]), vmax=np.max(x[:]), cmap="RdBu_r")
-    mpl.gca().set_aspect(1)
-    mpl.title("Timestep %d" % i)
-mpl.show()
+    plt.subplot(int(np.sqrt(T)), T / int(np.sqrt(T))+1,i+1)
+    plt.pcolormesh(x[:, :, i], vmin=np.min(x[:]), vmax=np.max(x[:]), cmap="RdBu_r")
+    plt.gca().set_aspect(1)
+    plt.title("Timestep %d" % i)
+# plt.show()
 
 # Show a timeseries plot
-mpl.plot(x[x.shape[0]/2, x.shape[1]/2, :])
-mpl.title("Timeseries for one point")
-mpl.xlabel("Time (h)")
-mpl.ylabel("Wind speed (m/s)")
-mpl.show()
+# mpl.plot(x[x.shape[0]/2, x.shape[1]/2, :])
+fig, ax = plt.subplots()
+point_x = x.shape[0]//2
+point_y = x.shape[1]//2
+ax.plot(x[point_x, point_y, :], label='downscaled')
+ax.plot(raw_intp[point_x, point_y, :], label='raw_forecast')
+ax.set_title("Timeseries for one point")
+ax.set_xlabel("Time (h)")
+ax.set_ylabel("Wind speed (m/s)")
+
